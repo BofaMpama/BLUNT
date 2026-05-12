c6aa39bc766bab926eec9ee0689dca74ba61462e
Bofa Mpama <mpamabofa@gmail.com>
2026-05-12 02:54:39 +0100

    changes to meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,10 @@ import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://blunt-technology.netlify.app/'),
-  title: "BLUNT Technology — No Fluff. Just Tech.",
+  title: "BLUNT Technology — Built for Africa's next.",
   description: "BLUNT Technology trains, equips, and launches Africa's next wave of tech professionals. From beginner to deployed product.",
   openGraph: {
-    title: "BLUNT Technology — No Fluff. Just Tech.",
+    title: "BLUNT Technology — Built for Africa's next.",
     description: "BLUNT Technology trains, equips, and launches Africa's next wave of tech professionals. From beginner to deployed product.",
     url: 'https://blunt-technology.netlify.app/',
     siteName: 'BLUNT Technology',
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: 'BLUNT Technology - No Fluff. Just Tech.',
+    title: 'BLUNT Technology - Built for Africa\'s next.',
     description: 'BLUNT Technology trains, equips, and launches Africa\'s next wave of tech professionals. From beginner to deployed product.',
     images: ["/image/preview.png"], // same image as above
   },
